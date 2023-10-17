0e6b145f1075b2274162364c497b478e63073029
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2023-10-17 10:02:56 +0530

    Readme + extra features added

--- Projects/Calendar/script.js
+++ Projects/Calendar/script.js
@@ -50,3 +50,21 @@ prevNextIcon.forEach(icon => {
         renderCalendar(); // calling renderCalendar function
     });
 });
+
+// For dark mode
+const modeToggle = document.getElementById("mode-toggle");
+const bodyElements = document.querySelectorAll("body *");
+modeToggle.addEventListener("change", () => {
+    // put dark class on evelry element inside body tag
+    document.body.classList.toggle("dark");
+})
+
+const toggle = document.getElementById('toggleDark');
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-moon');
+    if(this.classList.toggle('bi-brightness-high-fill')){
+        toggle.style.color = "black";
+    }else{
+        toggle.style.color = "white";
+    }
+});
