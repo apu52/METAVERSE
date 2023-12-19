03c8717aa336bf2c43b02d85f92f2756610ab304
Sami3160 <110652247+Sami3160@users.noreply.github.com>
2023-12-19 21:58:44 +0530

    Omnifood website added

---
+++ Projects/omnifood/js/script.js
@@ -0,0 +1,60 @@
+/////////////////////////////////////////////////
+// Set current year
+
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+
+yearEl.textContent = currentYear;
+
+/////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    // Scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+
+    // Close the mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
+////////////////////////////////////////////////
+// Sticky header
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 720) {
+    this.document.body.classList.add("sticky");
+  } else {
+    this.document.body.classList.remove("sticky");
+  }
+});
