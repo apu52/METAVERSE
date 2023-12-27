4d0dd6a8ccd635da00947cc68d2ba11a343de4a8
YashSachan2 <yash.sachan.ece22@itbhu.ac.in>
2023-12-28 05:25:22 +0530

    Added Crunchyroll Clone

--- Projects/Crunchyroll-Clone/src/components/Header.jsx
+++ Projects/Crunchyroll-Clone/src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header>
       <h1>
-        <strong>Chrunchyroll</strong>
+        <strong>Crunchyroll</strong>
         <SocialMedia />
       </h1>
     </header>
