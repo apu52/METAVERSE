cec900da9ad65bf2e6ef07ddf3bc76c8e9646dfd
Suvendu Mohanty <b123141@iiit-bh.ac.in>
2024-06-06 19:39:53 +0530

    Updated the back button click event in app.js and add new button in index.html #744

--- Projects/Career-Crafters/app.js
+++ Projects/Career-Crafters/app.js
@@ -10,6 +10,7 @@ $(".menu-bar").on("click",function(){
     $(".sidebar").addClass("active")
 });
 
-$(".logo").on("click",function(){
+$(".btn").on("click",function(){
     $(".sidebar").removeClass("active")
 });
+
