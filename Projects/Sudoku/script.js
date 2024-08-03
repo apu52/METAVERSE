067cbe2a4c1121de95dc8ed8249159ea0b81de59
Ayushmaanagarwal1121 <agarwalayushmaan88@gmail.com>
2024-08-03 16:38:10 +0530

    Created working sudolu

--- Projects/Sudoku/script.js
+++ Projects/Sudoku/script.js
@@ -1,247 +1,129 @@
-document
-  .getElementById("sudoku-board")
-  .addEventListener("keyup", function (event) {
-    if (event.target && event.target.nodeName == "TD") {
-      var validNum = /[1-9]/;
-      var tdEl = event.target;
-      if (tdEl.innerText.length > 0 && validNum.test(tdEl.innerText[0])) {
-        tdEl.innerText = tdEl.innerText[0];
-      } else {
-        tdEl.innerText = "";
-      }
+// Define the size of the Sudoku grid
+const gridSize = 9;
+const numPreFilled = 20; // Number of cells to pre-fill
+
+let puzzle = [];
+let solution = [];
+
+// Function to generate a random Sudoku board with some cells filled
+function generateRandomPuzzle() {
+    const puzzle = Array.from({ length: gridSize }, () => Array(gridSize).fill(0));
+    let count = 0;
+    while (count < numPreFilled) {
+        const row = Math.floor(Math.random() * gridSize);
+        const col = Math.floor(Math.random() * gridSize);
+        if (puzzle[row][col] === 0) {
+            const num = Math.floor(Math.random() * 9) + 1;
+            if (isValid(puzzle, row, col, num)) {
+                puzzle[row][col] = num;
+                count++;
+            }
+        }
     }
-  });
-
-document
-  .getElementById("solve-button")
-  .addEventListener("click", function (event) {
-    var boardString = boardToString();
-    var solution = SudokuSolver.solve(boardString);
-    if (solution) {
-      stringToBoard(solution);
-    } else {
-      alert("Invalid board!");
-    }
-  });
-
-document.getElementById("clear-button").addEventListener("click", clearBoard);
-
-function clearBoard() {
-  var tds = document.getElementsByTagName("td");
-  for (var i = 0; i < tds.length; i++) {
-    tds[i].innerText = "";
-  }
+    return puzzle;
 }
 
-function boardToString() {
-  var string = "";
-  var validNum = /[1-9]/;
-  var tds = document.getElementsByTagName("td");
-  for (var i = 0; i < tds.length; i++) {
-    if (validNum.test(tds[i].innerText[0])) {
-      string += tds[i].innerText;
-    } else {
-      string += "-";
-    }
-  }
-  return string;
-}
+// Function to solve the Sudoku puzzle using backtracking
+function solvePuzzle(puzzle) {
+    const emptyCell = findEmptyCell(puzzle);
+    if (!emptyCell) return true; // Puzzle solved
 
-function stringToBoard(string) {
-  var currentCell;
-  var validNum = /[1-9]/;
-  var cells = string.split("");
-  var tds = document.getElementsByTagName("td");
-  for (var i = 0; i < tds.length; i++) {
-    currentCell = cells.shift();
-    if (validNum.test(currentCell)) {
-      tds[i].innerText = currentCell;
-    }
-  }
-}
-
-("use strict");
-
-var EASY_PUZZLE =
-  "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--";
-var MEDIUM_PUZZLE =
-  "-3-5--8-45-42---1---8--9---79-8-61-3-----54---5------78-----7-2---7-46--61-3--5--";
-var HARD_PUZZLE =
-  "8----------36------7--9-2---5---7-------457-----1---3---1----68--85---1--9----4--";
-
-var TESTABLE = true;
-
-var SudokuSolver = (function (testable) {
-  var solver;
-
-  function solve(boardString) {
-    var boardArray = boardString.split("");
-    if (boardIsInvalid(boardArray)) {
-      return false;
-    }
-    return recursiveSolve(boardString);
-  }
-
-  function solveAndPrint(boardString) {
-    var solvedBoard = solve(boardString);
-    console.log(toString(solvedBoard.split("")));
-    return solvedBoard;
-  }
-
-  function recursiveSolve(boardString) {
-    var boardArray = boardString.split("");
-    if (boardIsSolved(boardArray)) {
-      return boardArray.join("");
-    }
-    var cellPossibilities = getNextCellAndPossibilities(boardArray);
-    var nextUnsolvedCellIndex = cellPossibilities.index;
-    var possibilities = cellPossibilities.choices;
-    for (var i = 0; i < possibilities.length; i++) {
-      boardArray[nextUnsolvedCellIndex] = possibilities[i];
-      var solvedBoard = recursiveSolve(boardArray.join(""));
-      if (solvedBoard) {
-        return solvedBoard;
-      }
+    const [row, col] = emptyCell;
+    for (let num = 1; num <= 9; num++) {
+        if (isValid(puzzle, row, col, num)) {
+            puzzle[row][col] = num;
+            if (solvePuzzle(puzzle)) return true;
+            puzzle[row][col] = 0; // Backtrack
+        }
     }
     return false;
-  }
+}
 
-  function boardIsInvalid(boardArray) {
-    return !boardIsValid(boardArray);
-  }
-
-  function boardIsValid(boardArray) {
-    return (
-      allRowsValid(boardArray) &&
-      allColumnsValid(boardArray) &&
-      allBoxesValid(boardArray)
-    );
-  }
-
-  function boardIsSolved(boardArray) {
-    for (var i = 0; i < boardArray.length; i++) {
-      if (boardArray[i] === "-") {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  function getNextCellAndPossibilities(boardArray) {
-    for (var i = 0; i < boardArray.length; i++) {
-      if (boardArray[i] === "-") {
-        var existingValues = getAllIntersections(boardArray, i);
-        var choices = ["1", "2", "3", "4", "5", "6", "7", "8", "9"].filter(
-          function (num) {
-            return existingValues.indexOf(num) < 0;
-          }
-        );
-        return { index: i, choices: choices };
-      }
-    }
-  }
-
-  function getAllIntersections(boardArray, i) {
-    return getRow(boardArray, i)
-      .concat(getColumn(boardArray, i))
-      .concat(getBox(boardArray, i));
-  }
-
-  function allRowsValid(boardArray) {
-    return [0, 9, 18, 27, 36, 45, 54, 63, 72]
-      .map(function (i) {
-        return getRow(boardArray, i);
-      })
-      .reduce(function (validity, row) {
-        return collectionIsValid(row) && validity;
-      }, true);
-  }
-
-  function getRow(boardArray, i) {
-    var startingEl = Math.floor(i / 9) * 9;
-    return boardArray.slice(startingEl, startingEl + 9);
-  }
-
-  function allColumnsValid(boardArray) {
-    return [0, 1, 2, 3, 4, 5, 6, 7, 8]
-      .map(function (i) {
-        return getColumn(boardArray, i);
-      })
-      .reduce(function (validity, row) {
-        return collectionIsValid(row) && validity;
-      }, true);
-  }
-
-  function getColumn(boardArray, i) {
-    var startingEl = Math.floor(i % 9);
-    return [0, 1, 2, 3, 4, 5, 6, 7, 8].map(function (num) {
-      return boardArray[startingEl + num * 9];
-    });
-  }
-
-  function allBoxesValid(boardArray) {
-    return [0, 3, 6, 27, 30, 33, 54, 57, 60]
-      .map(function (i) {
-        return getBox(boardArray, i);
-      })
-      .reduce(function (validity, row) {
-        return collectionIsValid(row) && validity;
-      }, true);
-  }
-
-  function getBox(boardArray, i) {
-    var boxCol = Math.floor(i / 3) % 3;
-    var boxRow = Math.floor(i / 27);
-    var startingIndex = boxCol * 3 + boxRow * 27;
-    return [0, 1, 2, 9, 10, 11, 18, 19, 20].map(function (num) {
-      return boardArray[startingIndex + num];
-    });
-  }
-
-  function collectionIsValid(collection) {
-    var numCounts = {};
-    for (var i = 0; i < collection.length; i++) {
-      if (collection[i] != "-") {
-        if (numCounts[collection[i]] === undefined) {
-          numCounts[collection[i]] = 1;
-        } else {
-          return false;
+// Helper function to find the next empty cell
+function findEmptyCell(puzzle) {
+    for (let row = 0; row < gridSize; row++) {
+        for (let col = 0; col < gridSize; col++) {
+            if (puzzle[row][col] === 0) return [row, col];
         }
-      }
+    }
+    return null;
+}
+
+// Function to check if a number can be placed in a cell
+function isValid(puzzle, row, col, num) {
+    if (puzzle[row].includes(num)) return false;
+    for (let i = 0; i < gridSize; i++) {
+        if (puzzle[i][col] === num) return false;
+    }
+    const boxRowStart = Math.floor(row / 3) * 3;
+    const boxColStart = Math.floor(col / 3) * 3;
+    for (let r = boxRowStart; r < boxRowStart + 3; r++) {
+        for (let c = boxColStart; c < boxColStart + 3; c++) {
+            if (puzzle[r][c] === num) return false;
+        }
     }
     return true;
-  }
+}
 
-  function toString(boardArray) {
-    return [0, 9, 18, 27, 36, 45, 54, 63, 72]
-      .map(function (i) {
-        return getRow(boardArray, i).join(" ");
-      })
-      .join("\n");
-  }
+// Function to create the Sudoku board
+function createSudokuBoard() {
+    const board = document.getElementById('sudoku-board');
+    puzzle = generateRandomPuzzle();
+    solution = JSON.parse(JSON.stringify(puzzle)); // Copy the puzzle for solution purposes
+    solvePuzzle(solution);
 
-  if (testable) {
-    solver = {
-      solve: solve,
-      solveAndPrint: solveAndPrint,
-      recursiveSolve: recursiveSolve,
-      boardIsInvalid: boardIsInvalid,
-      boardIsValid: boardIsValid,
-      boardIsSolved: boardIsSolved,
-      getNextCellAndPossibilities: getNextCellAndPossibilities,
-      getAllIntersections: getAllIntersections,
-      allRowsValid: allRowsValid,
-      getRow: getRow,
-      allColumnsValid: allColumnsValid,
-      getColumn: getColumn,
-      allBoxesValid: allBoxesValid,
-      getBox: getBox,
-      collectionIsValid: collectionIsValid,
-      toString: toString,
-    };
-  } else {
-    solver = { solve: solve, solveAndPrint: solveAndPrint };
-  }
+    while (board.firstChild) {
+        board.removeChild(board.firstChild);
+    }
 
-  return solver;
-})(TESTABLE);
+    for (let row = 0; row < gridSize; row++) {
+        for (let col = 0; col < gridSize; col++) {
+            const cell = document.createElement('input');
+            cell.type = 'text';
+            cell.maxLength = 1;
+            cell.className = 'sudoku-cell';
+
+            const value = puzzle[row][col];
+            if (value !== 0) {
+                cell.value = value;
+                cell.disabled = true;
+                cell.style.backgroundColor = '#e0e0e0'; // Light gray background for filled cells
+            } else {
+                cell.addEventListener('input', function (event) {
+                    if (event.target.value && !/^[1-9]$/.test(event.target.value)) {
+                        event.target.value = '';
+                    }
+                });
+            }
+
+            board.appendChild(cell);
+        }
+    }
+}
+
+// Function to reveal the solution
+function showSolution() {
+    const cells = document.querySelectorAll('#sudoku-board .sudoku-cell');
+    cells.forEach((cell, index) => {
+        if (!cell.disabled) {
+            cell.value = solution[Math.floor(index / gridSize)][index % gridSize];
+        }
+    });
+}
+
+// Function to clear the board
+function clearBoard() {
+    const cells = document.querySelectorAll('#sudoku-board .sudoku-cell');
+    cells.forEach(cell => {
+        if (!cell.disabled) {
+            cell.value = '';
+        }
+    });
+}
+
+// Initialize the Sudoku board and add event listeners on page load
+window.onload = function() {
+    createSudokuBoard();
+    document.getElementById('show-answer').addEventListener('click', showSolution);
+    document.getElementById('clear-board').addEventListener('click', clearBoard);
+};
